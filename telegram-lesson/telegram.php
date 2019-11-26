de451ab0a94d144364f899d91b256d164ec26c3b
Artyom Sazonov <55507012+artiomsazonov@users.noreply.github.com>
2019-11-26 15:36:58 +0300

    Add files via upload

--- telegram-lesson/telegram.php
+++ telegram-lesson/telegram.php
@@ -18,7 +18,7 @@ foreach($arr as $key => $value) {
   $txt .= "<b>".$key."</b> ".$value."%0A";
 };
 
-$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text={$txt}");
 
 if ($sendToTelegram) {
   header('Location: thank-you.html');
